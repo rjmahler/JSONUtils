3921b08bff73dd49557dbcd2c1b6b159a95418b7
Richard Joaquin Mahler <richard.j.mahler@gmail.com>
2016-11-28 18:37:19 -0800

    Add files via upload
    Initial commit.

---
+++ src/main/java/com/rjmahler/json/JsonUtils.java
@@ -0,0 +1,108 @@
+package com.rjmahler.json;
+
+import java.util.Iterator;
+import java.util.Map.Entry;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+
+/**
+ * @author rjmahler
+ */
+public class JsonUtils {
+	private JsonUtils() {
+	}
+	
+	/**
+	 * Gets the full path for a specific value from a JSON tree.
+	 * This only works for String values.
+	 * 
+	 * @param rootNode - JSON tree
+	 * @param path - current JSON path
+	 * @param value - requested search value
+	 * @return the string containing the full JSON path or null if no such value exists
+	 */
+	public static String getFullPath(JsonNode rootNode, String path, String value) {
+		if(rootNode == null || value == null) {
+			return null;
+		}
+
+		if(path == null) {
+			path = "";
+		}
+
+		JsonNodeType type = rootNode.getNodeType();
+		if(JsonNodeType.OBJECT.equals(type)) {
+			ObjectNode objectNode = (ObjectNode) rootNode;
+			Iterator<Entry<String, JsonNode>> iterator = objectNode.fields();
+			while(iterator.hasNext()) {
+				Entry<String,JsonNode> map = iterator.next();
+				String result = getFullPath(map.getValue(), path + "\\" + map.getKey(), value);
+				if(result != null) {
+					return result;
+				}
+			}
+		}
+		else if(JsonNodeType.ARRAY.equals(type)) {
+			ArrayNode arrayNode = (ArrayNode) rootNode;
+			for(int i = 0; i < arrayNode.size(); i++) {
+				String result = getFullPath(arrayNode.get(i), path + "[" + i + "]", value);
+				if(result != null) {
+					return result;
+				}
+			}
+		}
+		else if(JsonNodeType.STRING.equals(type)) {
+			TextNode st = (TextNode) rootNode;
+			if(st.asText().equals(value)) {
+				return path;
+			}
+		}
+
+		return null;
+	}
+	
+	/**
+	 * Replaces JSON data for a specific element in a JSON tree.
+	 * 
+	 * @param rootNode - JSON tree
+	 * @param element - element to replace
+	 * @param value - data replacing the element
+	 * @return true, if the operation is successful
+	 */
+	public static boolean findAndReplace(JsonNode rootNode, String element, JsonNode value) {
+		if(rootNode == null || element == null) {
+			return false;
+		}
+		JsonNodeType type = rootNode.getNodeType();
+		if(JsonNodeType.OBJECT.equals(type)) {
+			ObjectNode objectNode = (ObjectNode) rootNode;
+			if(objectNode.has(element)) {
+				objectNode.remove(element);
+				objectNode.setAll((ObjectNode)value);
+				return true;
+			}
+			else {
+				Iterator<Entry<String, JsonNode>> iterator = objectNode.fields();
+				while(iterator.hasNext()) {
+					Entry<String,JsonNode> map = iterator.next();
+					if(findAndReplace(map.getValue(), element, value)) {
+						return true;
+					}
+				}
+			}
+		}
+		else if(JsonNodeType.ARRAY.equals(type)) {
+			ArrayNode arrayNode = (ArrayNode) rootNode;
+			for(int i = 0; i < arrayNode.size(); i++) {
+				if(findAndReplace(arrayNode.get(i), element, value)) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+}
---
+++ src/test/java/com/rjmahler/json/JsonUtilsTest.java
@@ -0,0 +1,79 @@
+package com.rjmahler.json;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for JsonUtils
+ */
+public class JsonUtilsTest extends TestCase
+{
+	ObjectMapper objMapper;
+	
+	@Override
+	public void setUp() {
+		objMapper = new ObjectMapper();
+	}
+	
+	public void testFullPath() {
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader("jsontest1.txt"));
+			JsonNode rootNode = objMapper.readValue(br,  JsonNode.class);
+			assertEquals(JsonUtils.getFullPath(rootNode, "", "item2"), "\\itemList\\items[1]\\id");
+			assertEquals(JsonUtils.getFullPath(rootNode, "", "SubItem 3"),"\\itemList\\items[5]\\subItems[2]\\label");
+			assertEquals(JsonUtils.getFullPath(rootNode, "", "SubItem 2 item1"),"\\itemList\\items[8]\\subItems\\label");
+			assertNull(JsonUtils.getFullPath(rootNode, "", "SubItem 3 item1"));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+    public void testFindAndReplace() {
+    	BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader("jsontest2.txt"));
+			JsonNode rootNode = objMapper.readValue(br,  JsonNode.class);
+			br.close();
+			
+			br = new BufferedReader(new FileReader("personalinfo-phone.txt"));
+			JsonNode phoneNode = objMapper.readValue(br, JsonNode.class);
+			br.close();
+			assertTrue(JsonUtils.findAndReplace(rootNode, "serverInsert", phoneNode));
+			assertNotNull(JsonUtils.getFullPath(rootNode, "", "subItem1Itemphone"));
+				
+			br = new BufferedReader(new FileReader("personalinfo-ssn.txt"));
+			JsonNode ssnNode = objMapper.readValue(br, JsonNode.class);
+			br.close();
+			assertTrue(JsonUtils.findAndReplace(rootNode, "serverInsert", ssnNode));
+			assertNotNull(JsonUtils.getFullPath(rootNode, "", "subItem1Itemssn"));
+			assertNull(JsonUtils.getFullPath(rootNode, "", "serverInsert"));
+			System.out.println(rootNode);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+    }
+}
